80e98f740230b4acca5232daf79841c72cc29f3f
Firojpaudel <98528410+Firojpaudel@users.noreply.github.com>
2023-10-31 14:37:04 +0000

    writing to a file

--- trial.cpp
+++ trial.cpp
@@ -1,12 +1,28 @@
-// Example for writing to a disk file 
+// // Example for writing to a disk file 
+// #include<iostream>
+// #include<fstream>
+// using namespace std;
+
+// int main()
+// {
+//     ofstream outf;
+//     outf.open("trial.txt");
+//     outf<<"File demonstration program \n";
+//     outf<<"These are written into the file.";
+// }
+
+//Writing data to a file 
 #include<iostream>
 #include<fstream>
 using namespace std;
 
 int main()
 {
-    ofstream outf;
-    outf.open("trial.txt");
-    outf<<"File demonstration program \n";
-    outf<<"These are written into the file.";
+char ch='c';
+int i= 70;
+float f = 6.5;
+char *str="Patan";
+ofstream fout("Test.data");
+fout<<ch<<' '<<' '<<f<<' '<<str;
+cout<<"Data written to file\n";
 }
