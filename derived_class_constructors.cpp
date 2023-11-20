e6810f4d2b7ad9f8f5aa1df9586bad410269aa6c
Firojpaudel <98528410+Firojpaudel@users.noreply.github.com>
2023-11-20 12:53:13 +0000

    Refactor derived class constructors

--- derived_class_constructors.cpp
+++ derived_class_constructors.cpp
@@ -1,30 +1,75 @@
-//Derived class Constructor
+// //Derived class Constructor
+// #include<iostream>
+// using namespace std;
+
+// class A{
+//     protected:
+//         int adata;
+//     public:
+//         A(int a){
+//             adata = a;
+//         }
+// };
+
+// class B: public A{
+//     int bdata;
+//     public:
+//         B(int a, int b): A(a){
+//             bdata = b;
+//         }
+
+//         void showdata(){
+//             cout<<"adata="<<adata<<endl<<"bdata="<<bdata<<endl;
+//         }
+// };
+
+// int main(){
+//     B b(5,6);
+//     b.showdata();
+//     return 0;
+// }
+
+//Next Example:
+
 #include<iostream>
 using namespace std;
 
-class A{
+class A{ //Base Class
     protected:
-        int adata;
+        int count;
     public:
-        A(int a){
-            adata = a;
+        A(){ //zero Arguement Constructor : NO derived class constructor needed
+            count= 0;
+        }
+        A(int a){ //Constructor with arguement : Derived class Constructor required
+            count = a;
+        }
+        void display(){
+            cout<<"count: "<<count<<endl;
+        }
+        void operator ++(){
+            count++;
         }
 };
 
 class B: public A{
-    int bdata;
     public:
-        B(int a, int b): A(a){
-            bdata = b;
+        B():A(){} //Explicit type invocation
+        B(int b): A(b){ //Derived Class Constructor
+            count =b;
         }
-
-        void showdata(){
-            cout<<"adata="<<adata<<endl<<"bdata="<<bdata<<endl;
+        void operator --() {
+            count--;
         }
 };
 
 int main(){
-    B b(5,6);
-    b.showdata();
+    B i1;
+    B i2(200);
+    ++i1;
+    ++i1;
+    i1.display();
+    --i2;
+    i2.display();
     return 0;
 }
